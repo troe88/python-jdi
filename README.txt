1345a1a22df93788b30076269cef96450be6baab
Dmitry_Lebedev1 <Dmitry_Lebedev1@epam.com>
2018-05-24 17:44:54 +0300

    refactoring

--- README.txt
+++ README.txt
@@ -2,3 +2,8 @@ Description
 ===========
 
 Template project for web ui automation
+
+===========
+
+pytest -m jdi_criticalPath --domain=qa --html=report_crtcl.html
+pytest -m jdi_smoke --domain=qa --html=report_smoke.html
--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-    test_suite='tests.test_Classes',
     entry_points={
         'console_scripts':
             []
